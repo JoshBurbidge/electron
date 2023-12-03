9e1beadf24df148796275dff2db4fcacee081113
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2023-12-02 20:36:53 -0600

    starting panels

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,58 @@
 import * as React from 'react';
 import Request from './Request/Request.jsx';
-import { Typography } from '@mui/material';
+import { Box } from '@mui/material';
 
 const App = () => {
+  const [dragging, setDragging] = React.useState(false);
+  const [height, setHeight] = React.useState(50);
+
+  React.useEffect(() => {
+    window.addEventListener('mouseup', () => {
+      if (dragging) {
+        setDragging(false);
+      }
+    });
+
+    window.addEventListener('mousemove', (e) => {
+      if (dragging) {
+        // console.log(e);
+      }
+    });
+  });
+
+  console.log(dragging);
+
+  const handleDrag = (e) => {
+    console.log(e);
+    setDragging(true);
+  };
+
   return (
-    <div>
-      {/* eslint-disable-next-line no-undef */}
-      <Typography>{`Chrome version ${versions.chrome()}`}</Typography>
-      <Request></Request>
-    </div>
+    <Box height={'100vh'} display={'flex'} flexDirection={'column'}>
+      <Request />
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        flex: '1'
+      }}>
+        <Box bgcolor={'lightblue'} flex={'1'}>{'request'}</Box>
+        <Box sx={{
+          '&:hover': {
+            cursor: 'ns-resize'
+          },
+          maxHeight: '16px',
+          bgcolor: 'gray',
+        }} onMouseDown={handleDrag}>
+        test
+        </Box>
+        <Box bgcolor={'lightgray'} height={height}>{'response'}</Box>
+      </Box>
+    </Box>
   );
 };
 
 export default App;
+
+// TODO
+// some kind of height calculation
+// resize on mousemove
