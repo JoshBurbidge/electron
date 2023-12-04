78577e97656ac0969a1ded015b744bc252b65132
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2023-12-03 19:55:25 -0600

    draggable border

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,38 @@ import Request from './Request/Request.jsx';
 import { Box } from '@mui/material';
 
 const App = () => {
-  const [dragging, setDragging] = React.useState(false);
+  const [dragging, _setDragging] = React.useState(false);
   const [height, setHeight] = React.useState(50);
 
-  React.useEffect(() => {
-    window.addEventListener('mouseup', () => {
-      if (dragging) {
-        setDragging(false);
-      }
-    });
+  // idk if these refs are necessary
+  const draggingRef = React.useRef(dragging);
 
-    window.addEventListener('mousemove', (e) => {
-      if (dragging) {
-        // console.log(e);
-      }
-    });
-  });
-
-  console.log(dragging);
-
-  const handleDrag = (e) => {
-    console.log(e);
-    setDragging(true);
+  const setDragging = (dragging) => {
+    draggingRef.current = dragging;
+    _setDragging(dragging);
   };
+
+  const stopDragging = () => {
+    setDragging(false);
+  };
+
+  const handleMove = (e) => {
+    if (draggingRef.current) {
+      setHeight(e.view.innerHeight - e.clientY);
+    }
+  };
+
+  React.useEffect(() => {
+    window.addEventListener('mouseup', stopDragging);
+    window.addEventListener('mousemove', handleMove);
+
+    return () => {
+      window.removeEventListener('mouseup', stopDragging);
+      window.removeEventListener('mousemove', handleMove);
+    };
+  }, []);
+
+  // console.log(draggingRef.current);
 
   return (
     <Box height={'100vh'} display={'flex'} flexDirection={'column'}>
@@ -35,14 +44,17 @@ const App = () => {
         flexDirection: 'column',
         flex: '1'
       }}>
-        <Box bgcolor={'lightblue'} flex={'1'}>{'request'}</Box>
+        <Box bgcolor={'lightblue'} flex={'1'}>
+          {'request'}
+          {/* {dragging && ' dragging'} */}
+        </Box>
         <Box sx={{
           '&:hover': {
             cursor: 'ns-resize'
           },
           maxHeight: '16px',
           bgcolor: 'gray',
-        }} onMouseDown={handleDrag}>
+        }} onMouseDown={() => setDragging(true)}>
         test
         </Box>
         <Box bgcolor={'lightgray'} height={height}>{'response'}</Box>
